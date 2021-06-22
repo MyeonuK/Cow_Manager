f320a3e131b72fe5c52abe4a1edb49c019369c60
MyeonuK <gusdn0217@gmail.com>
2021-06-22 23:42:02 +0900

    restart

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.get("/", (req, res) => {
   //res.render("index");
-  console.log("yeae");
+  console.log("wtff");
 });
 app.listen(PORT, () => {
   console.log(`server started on PORT ${PORT}`);
