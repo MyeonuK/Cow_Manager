b389d01bbd0b7b27b52a6dace33f87da71b9b085
MyeonuK <gusdn0217@gmail.com>
2021-06-28 19:39:21 +0900

    Fixing error - ERR_ABORTED 404 (Not Found)

--- web.js
+++ web.js
@@ -1,28 +1,23 @@
 console.log("hello");
-/*
+
 const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 8001;
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.set(
+  "views",
+  path.join("/home/hosting_users/gusdn0217/apps/myeonu", "/views")
+);
+app.use(
+  "/",
+  express.static(
+    path.join("/home/hosting_users/gusdn0217/apps/myeonu", "/public")
+  )
+);
 app.get("/", (req, res) => {
   console.log("???");
   res.render("index.ejs");
-});
-app.listen(PORT, () => {
-  console.log(`server started on PORT ${PORT}`);
-});
-*/
-const express = require("express");
-const app = express();
-const PORT = 8001;
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
-app.use(express.static(path.join(__dirname + "/public")));
-app.get("/", (req, res) => {
-  res.render("index");
 });
 app.listen(PORT, () => {
   console.log(`server started on PORT ${PORT}`);
