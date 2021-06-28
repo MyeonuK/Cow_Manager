f8491d7abfaf152849ad3427f68fbdbcac9229f3
MyeonuK <gusdn0217@gmail.com>
2021-06-28 19:09:04 +0900

    Update

--- web.js
+++ web.js
@@ -6,6 +6,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use("/", express.static(path.join(__dirname, "/public")));
 app.get("/", (req, res) => {
+  console.log("???");
   res.render("index");
 });
 app.listen(PORT, () => {
