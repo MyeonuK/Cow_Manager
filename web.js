e76ff26b3814de4c74919c54ac1f493886d0c509
MyeonuK <gusdn0217@gmail.com>
2021-06-28 19:44:40 +0900

    Fixing error - ERR_ABORTED 404 (Not Found)

--- web.js
+++ web.js
@@ -5,15 +5,10 @@ const path = require("path");
 const app = express();
 const PORT = 8001;
 app.set("view engine", "ejs");
-app.set(
-  "views",
-  path.join("/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu", "/views")
-);
+app.set("views", "/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu/views");
 app.use(
   "/",
-  express.static(
-    path.join("/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu", "/public")
-  )
+  express.static("/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu/public")
 );
 app.get("/", (req, res) => {
   console.log("???");
