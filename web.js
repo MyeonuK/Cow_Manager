d4fb69ba4d0e6cb8d8a59d5746ca0898c0432200
MyeonuK <gusdn0217@gmail.com>
2021-06-30 15:37:35 +0900

    Change files for api module

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const path = require("path");
 
-//const indexRouter = require("/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu/routes/index");
-const indexRouter = require("./routes/index");
-//const indexRouter = require("/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu/routes/users");
-const userRouter = require("./routes/users");
+const indexRouter = require("/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu/routes/index");
+//const indexRouter = require("./routes/index");
+const userRouter = require("/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu/routes/users");
+//const userRouter = require("./routes/users");
 
 const app = express();
 const PORT = 8001;
 
 // view engine setup
-//app.set("views", "/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu/views");
-app.set("views", path.join(__dirname, "./views"));
+app.set("views", "/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu/views");
+//app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
 
 /*
