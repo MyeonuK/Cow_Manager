bc761414b1b0c9b566d6279e1bff42e3236d4882
MyeonuK <gusdn0217@gmail.com>
2021-07-15 22:46:56 +0900

    trying make db and api

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const cors = requird("cors");
+const cors = require("cors");
 
 const indexRouter = require("/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu/routes/index");
 //const indexRouter = require("./routes/index");
