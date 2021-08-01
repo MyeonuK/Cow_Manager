be185d06570dc7323c7f55314ae8eadb8c95c55f
MyeonuK <gusdn0217@gmail.com>
2021-08-02 04:59:05 +0900

    api 오류 수정

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
-const db = require("./database");
+//const db = require("./database");
 //const crawler = require("./crawler");
 const router = express.Router();
+const mysql = require("mysql");
+
+let dbInfo = {
+  host: "myeonu.cafe24app.com",
+  user: "gusdn0217",
+  password: "Dbdb4783!",
+  database: "gusdn0217",
+  port: "3306",
+};
 
 router.get("/", function (req, res) {
   res.render("index.ejs");
@@ -10,12 +19,30 @@ router.get("/", function (req, res) {
 router.get("/update", function (req, res) {
   // db.update(req, res, conn);
 });
-
+/*
 router.get("/load", function (req, res) {
   let io = db.load(req.query.id);
   console.log(io);
   //res.send(JSON.stringify(db.load(req.query.id)));
   //res.status(200).json(db.load(req.query.id));
+});
+*/
+
+router.get("/load", function (req, res) {
+  let sql = `SELECT * FROM cowList WHERE id="${req.query.id}"`;
+
+  if (req.query.id == undefined) {
+    sql = `SELECT * FROM cowList`;
+  }
+  console.log(sql);
+
+  conn.query(sql, function (err, rows, fields) {
+    if (err) {
+      console.error(err);
+    } else {
+      res.status(200).json(rows);
+    }
+  });
 });
 
 module.exports = router;
