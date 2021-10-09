70438f9f53c698be408266179d2b38111ff39737
MyeonuK <gusdn0217@gmail.com>
2021-10-09 15:48:22 +0900

    배포

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-/*
 const conn = mysql.createConnection({
   host: "myeonu.cafe24app.com",
   user: "gusdn0217",
@@ -13,7 +12,7 @@ const conn = mysql.createConnection({
   database: "gusdn0217",
   port: "3306",
 });
-*/
+/*
 const conn = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -21,7 +20,7 @@ const conn = mysql.createConnection({
   database: "cowmanager",
   port: "3306",
 });
-
+*/
 function writeLog(message) {
   log = `=========${new Date()}=========\n${message}\n\n`;
   fs.appendFile("public/log/newDB.txt", log, function (err) {
