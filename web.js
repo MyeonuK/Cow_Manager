7f1ed478a41d2997904ada8411b841942092675e
MyeonuK <gusdn0217@gmail.com>
2021-11-18 00:38:38 +0900

    배포

--- web.js
+++ web.js
@@ -9,21 +9,20 @@ const app = express();
 const PORT = 8001;
 
 // view engine setup (cafe24)
-/*
+
 app.set("view engine", "ejs");
 app.set("views", "/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu/views");
-
 app.use(
   "/",
   express.static("/home/hosting_users/gusdn0217/apps/gusdn0217_myeonu/public")
 );
-*/
-// view engine setup (local)
 
+// view engine setup (local)
+/*
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 app.use("/", express.static(path.join(__dirname, "./public")));
-
+*/
 //
 app.use("/", indexRouter);
 app.use(cors());
